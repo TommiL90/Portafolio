3c524323e14f1bfbcbc94157228f01d934f1fb6d
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-05-15 22:08:07 -0300

    feat: create `mvp` page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GlobalStyle } from "@/styles/Global";
+import { Home } from "@/pages/home";
+import { NavBar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyle>
+      <NavBar />
+      <Home />
+      <Footer />
+    </GlobalStyle>
+  </React.StrictMode>
+);
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Tomás Lillo S.",
+  githubUser: "tommil90",
+  linkedinUser: "tomasbenjamin",
+  whatsappNumber: "+5548991295746",
+  emailUser: "tomasbenjamin.l90@gmail.com",
+  instagramUser: "",
+  facebookUser: "",
+};
